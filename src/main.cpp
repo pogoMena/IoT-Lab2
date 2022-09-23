7323566e57ea1236521964290af0411d86cbe4cd
Mena <000817498@mohawkcollege.ca>
2022-09-23 11:08:06 -0400

    Added statement of authorship

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
 #include <Arduino.h>
 
+/*
+"StAuth10222: I Michael Mena, 000817498 certify that this material is my original work. 
+No other person's work has been used without due acknowledgement. I have not made my work available to anyone else."
+*/
+
 void setup()
 {
   Serial.begin(115200);
